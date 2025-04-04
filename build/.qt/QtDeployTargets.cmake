f5322f21221ed750c00a342c48c41a5e56620f75
Dawid <dawid@Dawid.home>
2025-04-04 23:05:03 +0200

    Remove build/ directory from repository

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_QZTool_FILE /home/dawid/Code/qt/QZTool/build/QZTool)
-set(__QT_DEPLOY_TARGET_QZTool_TYPE EXECUTABLE)
--- build/Desktop_Qt_6_7_2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_QZTool_FILE /home/dawid/Code/qt/QZTool/build/Desktop_Qt_6_7_2-Debug/QZTool)
-set(__QT_DEPLOY_TARGET_QZTool_TYPE EXECUTABLE)
